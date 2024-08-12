943cddab79e10c9024a1157d32c28aa0cd2daa9a
nicu <ontanicolae15@gmail.com>
2024-08-12 17:13:54 +0300

    Swagger dark mode and global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('TrainSync')
     .setDescription('Workout tracker app')
     .setVersion('1.0')
-    .addTag('fitness')
+    // .addTag('fitness')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const theme = new SwaggerTheme();
+  const options = {
+    explorer: true,
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
+  };
+  SwaggerModule.setup('api', app, document, options);
 
   await app.listen(3000);
 }
